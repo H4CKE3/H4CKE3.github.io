cbf17ff5c07c2062112d4246082004c604197f8f
RE <2350299706@qq.com>
2023-09-07 22:32:53 +0800

    202397

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,15 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const githubListener = document.querySelector(".main .github");
-    const qqListener = document.querySelector(".main .qq");
-    const wechatListener = document.querySelector(".main .wechat");
 
-    githubListener.addEventListener("mouseup", function () {
-        window.open("https://github.com/H4CKE3/H4CKE3.github.io", "_blank")
+
+//跳转联系方式
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        const githubListener = document.querySelector(".main .github");
+        const qqListener = document.querySelector(".main .qq");
+        const wechatListener = document.querySelector(".main .wechat");
+
+        githubListener.addEventListener("mouseup", function () {
+            window.open("https://github.com/H4CKE3/H4CKE3.github.io", "_blank")
+        });
+        qqListener.addEventListener("mouseup", function () {
+            window.open("https://qm.qq.com/q/smfdtdHzh0&personal_qrcode_source=3", "_blank")
+        });
+        wechatListener.addEventListener("mouseup", function () {
+            window.open("../assets/wechatQR.png", "_blank")
+        });
     });
-    qqListener.addEventListener("mouseup", function () {
-        window.open("https://qm.qq.com/q/smfdtdHzh0&personal_qrcode_source=3", "_blank")
+})();
+
+
+
+
+
+///导航栏显示
+(function () {
+    window.addEventListener("scroll", function () {
+        const topBar = document.querySelector('.topBar');
+        const dis = document.documentElement.scrollTop;
+        if (dis >= 75) {
+
+        }
     });
-    wechatListener.addEventListener("mouseup", function () {
-        window.open("../assets/wechatQR.png", "_blank")
-    });
-});
+})();
