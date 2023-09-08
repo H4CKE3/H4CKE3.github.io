9038d852131482fcdbdc7f06812d947d1de04642
RE <2350299706@qq.com>
2023-09-08 22:48:33 +0800

    2023/9/8

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,10 +1,9 @@
-//跳转联系方式
-(function () {
-    document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
+    //跳转联系方式
+    (function () {
         const githubListener = document.querySelector(".main .github");
         const qqListener = document.querySelector(".main .qq");
         const wechatListener = document.querySelector(".main .wechat");
-
         githubListener.addEventListener("mouseup", function () {
             window.open("https://github.com/H4CKE3/H4CKE3.github.io", "_blank")
         });
@@ -14,59 +13,30 @@
         wechatListener.addEventListener("mouseup", function () {
             window.open("../assets/wechatQR.png", "_blank")
         });
-    });
-})();
+    })();
 
 
 
 
 
-// 跳转头部显示效果
-(function () {
-    window.addEventListener("scroll", function () {
-        const up = this.document.querySelector(".upToTop");
+    // 跳转头部
+    (function () {
+        const up = document.querySelector(".upToTop");
         const dis = document.documentElement.scrollTop;
-        if (dis >= 75) {
-            up.style.opacity = 1;
-            up.style.right = "15px";
-        } else {
-            up.style.opacity = 0;
-            up.style.right = "-15px";
-        }
-    });
-})();
-
-
-
-
-
-//跳转到头部
-window.onload = function () {
-    const up = this.document.querySelector(".upToTop");
-    console.log(up);
-    up.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        window.addEventListener("scroll", function () {
+            if (dis >= 75) {
+                up.style.opacity = 1;
+                up.style.right = "15px";
+            } else {
+                up.style.opacity = 0;
+                up.style.right = "-15px";
+            }
+        });
+        up.addEventListener("click", function () {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+    })();
+});
 
 
 
